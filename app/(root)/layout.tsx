10f03fdb31615701a8a8894a8e98d8f37e729820
Manoj Kande <manojkande333@gmail.com>
2024-12-13 17:49:03 +0530

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,31 @@
+import MobileNav from "@/components/MobileNav";
+import  Sidebar  from "@/components/Sidebar";
+import Image  from 'next/image'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  const loggedIn={firstName:"Manoj ",lastName:"Kande"};
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter" >
+        <Sidebar user={loggedIn} />
+
+        <div className='flex size-full flex-col' >
+          <div className="root-layout" >
+            <Image src="/icons/logo.svg" alt="menu  icon"
+            height={30} width={30}/>
+
+            <div>
+              <MobileNav user={loggedIn} />
+            </div>
+
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
+import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSideBar from "@/components/RightSideBar";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: "Manoj ",lastName:"Kande",email:"conatactme@gmail.com" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently. "
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1235.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+
+      </div>
+      <RightSideBar 
+       user={loggedIn}
+       transactions={[]}
+       banks={[{currentBalance:123.50},{currentBalance:500}]}
+       
+       />
+    </section>
+  );
+};
+
+export default Home;
